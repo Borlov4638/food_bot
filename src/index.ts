4fdaefe32245d95cc5c828b6196e8818bf08cc5e
orisborlov <mrwiggle40000@gmail.com>
2023-11-08 10:29:57 +0700

    added host ip

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import { botSetup } from "./bot";
     try {
         botSetup().launch({
             webhook: {
+                host: '80.90.179.53',
                 domain: 'https://1408935-cz73058.tw1.ru',
-                port: 443
+                port: 443,
             }
         })
     } catch (err) {
